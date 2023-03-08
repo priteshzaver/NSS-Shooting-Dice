f56ebb6b25511c5ed4cf80e9ed18c764f8346cf0
Pritesh Zaver <priteshzaver@gmail.com>
2023-03-08 12:17:56 -0600

    creative smack talking player

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -1,8 +1,60 @@
+using System;
+using System.Collections.Generic;
+
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
-    public class CreativeSmackTalkingPlayer
+    public class CreativeSmackTalkingPlayer : Player
     {
+        public string Taunt { get; set;}
+
+
+        public CreativeSmackTalkingPlayer()
+        {
+            RandomTaunt();
+        }
+
+        public override void Play(Player other)
+        {
+            // Call roll for "this" object and for the "other" object
+            int myRoll = Roll();
+            int otherRoll = other.Roll();
+
+            Console.WriteLine($"{Name} rolls a {myRoll} and shouts {Taunt}.");
+            Console.WriteLine($"{other.Name} rolls a {otherRoll}");
+            if (myRoll > otherRoll)
+            {
+                Console.WriteLine($"{Name} Wins!");
+            }
+            else if (myRoll < otherRoll)
+            {
+                Console.WriteLine($"{other.Name} Wins!");
+            }
+            else
+            {
+                // if the rolls are equal it's a tie
+                Console.WriteLine("It's a tie");
+            }
+        }
+
+        public void RandomTaunt()
+        {
+            List<string> allTaunts = new List<string> {
+                "You look like what morning breath smells like.",
+                "If you tried to give me cpr I would probably throw myself back under water",
+                "I am not a fan of you",
+                "You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
+                "I'd rather walk than be on the same plane as you",
+                "If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
+                "Do you have to be so...like that?",
+                "You roll like yo momma"
+            };
+            Random r = new Random();
+            int genRand = r.Next(0, 8);
+            Taunt = allTaunts[genRand];
+            
+        }
+
 
     }
 }
